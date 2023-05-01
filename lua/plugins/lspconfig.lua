9d7490fa25909096669bcae58a8a342a4384a831
Yuhuang Hu <duguyue100@gmail.com>
2023-05-01 21:37:06 +0200

    add a lua language server

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -11,15 +11,39 @@ return {
         },
         config = function()
             local capabilities = require(
-                'cmp_nvim_lsp'
+            'cmp_nvim_lsp'
             ).default_capabilities(
-                vim.lsp.protocol.make_client_capabilities()
+            vim.lsp.protocol.make_client_capabilities()
             )
             require('lspconfig').jedi_language_server.setup {
-              capabilities = capabilities
+                capabilities = capabilities
             }
 
             require'lspconfig'.tsserver.setup {capabilities = capabilities}
+
+            require'lspconfig'.lua_ls.setup{
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = {'vim'},
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            }
         end
     },
 }
