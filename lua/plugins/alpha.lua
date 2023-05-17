e1d3046a8ce953f90d3954a8a00e0afc9e733340
Yuhuang Hu <duguyue100@gmail.com>
2023-05-17 12:26:55 +0200

    fixing icons in the startup screens

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -29,6 +29,14 @@ return {
             [[          ~~                                                            ]],
             }
 
+            dashboard.section.buttons.val = {
+                dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("SPC f f", "  Find file", ":Telescope find_files<CR>"),
+                dashboard.button("SPC f h", "  Recently opened files", ":Telescope oldfiles<CR>"),
+                dashboard.button("SPC f g", "  Find word", ":Telescope oldfiles<CR>"),
+                dashboard.button("SPC f r", "  Frecency/MRU"),
+            }
+
             require'alpha'.setup(dashboard.config)
         end,
     },
