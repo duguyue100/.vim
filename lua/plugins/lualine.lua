3485eba1cccd34870620be1c8c375b7c21220e05
Yuhuang Hu <duguyue100@gmail.com>
2023-05-24 00:37:13 +0200

    improve visualization

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -20,7 +20,7 @@ return {
                     {'diff'},
                     {
                         'diagnostics',
-                        symbols = {error = '󰅝', warn = '󰅾', info = '󰋖', hint = '󰆉'}
+                        symbols = {error = '󰅝 ', warn = '󰅾 ', info = '󰋖 ', hint = '󰆉 '}
                     },
                 },
                 lualine_c = {'filename'},
