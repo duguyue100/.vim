dd389ee3a3068edba6a45121f2ef026e9290501f
Yuhuang Hu <duguyue100@gmail.com>
2023-05-24 00:35:42 +0200

    fixing icons

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -15,7 +15,14 @@ return {
             },
             sections = {
                 lualine_a = {'mode'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_b = {
+                    {'branch'},
+                    {'diff'},
+                    {
+                        'diagnostics',
+                        symbols = {error = '󰅝', warn = '󰅾', info = '󰋖', hint = '󰆉'}
+                    },
+                },
                 lualine_c = {'filename'},
                 lualine_x = {'encoding', 'fileformat', 'filetype'},
                 lualine_y = {'progress'},
