bffd00bc44d3bb3e9f2cbed69d62b52e98d427ca
Yuhuang Hu <duguyue100@gmail.com>
2024-01-23 12:41:12 +0100

    define my junkfile cmd

--- lua/plugins/junkfile.lua
+++ lua/plugins/junkfile.lua
@@ -2,5 +2,8 @@ return {
     {
         'Shougo/junkfile.vim',
         cmd = "JunkfileOpen",
+        config = function()
+            vim.cmd [[command! JunkfileOpen call junkfile#open(strftime('%Y-%m-%d-%H%M%S.'))]]
+        end,
     },
 }
