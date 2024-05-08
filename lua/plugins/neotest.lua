1742101ea271c2213b819370dc2ec0b033894156
Yuhuang Hu <duguyue100@gmail.com>
2024-05-08 13:21:57 +0200

    neotest introduced another dependency, fix it

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -16,6 +16,7 @@ return {
 			"nvim-neotest/neotest-python",
 			"nvim-neotest/neotest-plenary",
 			"nvim-neotest/neotest-vim-test",
+            "nvim-neotest/nvim-nio"
 		},
 		config = function()
 			require("neotest").setup({
