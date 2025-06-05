82e842f8c161e9c019cbb9c90febf0b19a26006f
Yuhuang Hu <yuhuang@latticeflow.ai>
2025-06-05 16:13:29 +0200

    adding homebrew specific commands

--- config.fish
+++ config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if test -d /opt/homebrew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
 set -U fish_color_command green
 set -g fish_key_bindings fish_vi_key_bindings
 set -U fish_greeting ""
