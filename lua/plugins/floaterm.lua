26b84941e1357308aed6fdb9323e44115e33e9fb
Yuhuang Hu <yuhuang@latticeflow.ai>
2025-06-05 16:35:18 +0200

    fixing slow start up of nvim

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -5,6 +5,7 @@ return {
         config = function()
             vim.g.floaterm_width = 0.8
             vim.g.floaterm_height = 0.8
+            vim.g.floaterm_shell = "fish"
         end,
     },
 }
