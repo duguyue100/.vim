530d2ad10e4d6b85e881ab5cf064e8629ecd096f
Yuhuang Hu <yuhuang@latticeflow.ai>
2026-02-05 18:41:04 +0100

    fixing

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -12,8 +12,9 @@ return {
         vim.g.opencode_opts = {
             -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
             provider = {
-                enabled = "terminal",
-                terminal = {
+                enabled = "tmux",
+                tmux = {
+                    options = "-h -t 1"
               -- ...
             }
           }
