dd279142f56203cc78848334678e366782227b3b
Yuhuang Hu <duguyue100@gmail.com>
2026-02-08 04:33:01 +0100

    update fish options

--- config.fish
+++ config.fish
@@ -6,6 +6,10 @@ if test -d /opt/homebrew
     eval (/opt/homebrew/bin/brew shellenv)
 end
 
+if test -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
 set -U fish_color_command green
 set -g fish_key_bindings fish_vi_key_bindings
 set -U fish_greeting ""
