16f062f25c3b20a4f597bc4d1c6cdd872d54e56f
Yuhuang Hu <yuhuang@latticeflow.ai>
2026-02-09 02:05:34 +0100

    using linuxbrew nvim

--- config.fish
+++ config.fish
@@ -15,7 +15,6 @@ set -g fish_key_bindings fish_vi_key_bindings
 set -U fish_greeting ""
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --preview 'cat {}'"
 
-export PATH="/opt/nvim-linux64/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 
 alias m=$HOME/.vim/macman/macman
