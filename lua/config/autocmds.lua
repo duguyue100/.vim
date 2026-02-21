481467d7912449532e44dd44d1e56e271ac13eea
Yuhuang Hu <duguyue100@gmail.com>
2026-02-21 18:58:10 +0100

    toggle global kill

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -58,8 +58,8 @@ vim.on_key(toggle_hlsearch, ns)
 
 -- Kill all opencode process on exit
 -- TODO: we can remove this if opencode.nvim merged the fix
-vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function()
-    vim.fn.jobstart({ "pkill", "-f", "opencode" }, { detach = true })
-  end,
-})
+-- vim.api.nvim_create_autocmd("VimLeavePre", {
+--   callback = function()
+--     vim.fn.jobstart({ "pkill", "-f", "opencode" }, { detach = true })
+--   end,
+-- })
